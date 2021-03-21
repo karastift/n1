8ba43ca9b1bd9365cf19d3ffe7cea1d90439d745
Kian van der Meer <kiansinan@gmail.com>
2021-03-21 09:30:22 +0100

    new

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = 'ODAzMzQ1NTg0ODI3NTMxMjY0.YA8b4A.5U0NfZPTM67TsNg0IKq_dLQBqbI';
+const token = 'ODAzMzQ1NTg0ODI3NTMxMjY0.YA8b4A.hYui2lNV02L6sA_f-goSR1nDfKM';
 
 var move = false;
 
@@ -39,23 +39,40 @@ function clearChat(n, message) {
     else {message.channel.send('Only deleted 100 messages, because you can\'t delete more than 100 messages.');}
 }
 
-function movingMain() {
-    var guildId = message.guild.id;
+function movingMain(message) {
+    /*
     var i = 0;
     while (move) {
-        if (i == 10000) {movingMover(movingMover()); i = 0;}
+        if (i == 10000) {
+            movingMover(movingListener());
+            i = 0;
+        }
         i++;
+    }*/
+    movingMover(movingListener(movingUsers(message)));
+}
+
+function movingListener(userList) {
+    let userGames = {};
+    for (u = 0; u < userList.length; u++) {
+        var game = userList[u].presence.game;
+        game.name = game.name.toString();
+        userGames[u] = u.presence.game;
+        return userGames;
     }
 }
 
-function movingListener(message, guildId) {
-    gamesOfUsers = {};
-    //console.log(message.guild.members.filter(member => member.presence.status === "online"));
-}
-
-function movingMover(dict) {
+function movingMover(userGames) {
     // get list of channel
     // move users into channels
+}
+
+function movingUsers(message) {
+    let userList = [];
+    const server = client.guilds.get(message.guild.id).id;
+    const list = client.guilds.get(server); 
+    list.members.forEach(member => userList.push(member.user.id));
+    return userList;
 }
 
 client.login(token);
